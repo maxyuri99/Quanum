5ce0202794f24bf1f49f81ff9edb7856e13d50b3
Max Yuri <maxyurisantos42@gmail.com>
2024-08-11 21:31:42 -0300

    Primeiro envio

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    devServer: {
+      disableHostCheck: true,
+      public: 'https://quanum-quanum.6yhsxt.easypanel.host/', // substitua pelo seu domínio ou subdomínio
+      host: '0.0.0.0', // permite que a aplicação seja acessível externamente
+    },
+  };
